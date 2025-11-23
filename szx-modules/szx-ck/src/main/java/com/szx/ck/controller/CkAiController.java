f4015e3722100aa4a61412a72905e9fa7f5e0899
Yan <3534358648@qq.com>
2025-11-23 13:55:16 +0800

    ai

---
+++ szx-modules/szx-ck/src/main/java/com/szx/ck/controller/CkAiController.java
@@ -0,0 +1,19 @@
+package com.szx.ck.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author
+ * @date 2025/11/23
+ */
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ck/ai")
+public class CkAiController {
+
+
+}
